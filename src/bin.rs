ce12f4879ac7f6b8d2ae7f1c5fbe9f03c06413ed
Benjamin Fry <benjamin.fry@salesforce.com>
2017-07-10 17:16:57 -0700

    add regex options

--- src/bin.rs
+++ src/bin.rs
@@ -1,10 +1,12 @@
 extern crate clap;
 extern crate colored;
+extern crate regex;
 
 use std::io::{stdin, stdout, BufRead, Write};
 
 use clap::*;
 use colored::Colorize;
+use regex::{Regex, RegexSet};
 
 fn main() {
     let matches = App::new(env!("CARGO_PKG_NAME"))
@@ -13,51 +15,60 @@ fn main() {
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             Arg::with_name("green")
-                .help("String to match for green")
+                .help("Regex to match for green")
                 .short("g")
                 .long("green")
                 .default_value("INFO"),
         )
         .arg(
             Arg::with_name("yellow")
-                .help("String to match for yellow")
+                .help("Regex to match for yellow")
                 .short("y")
                 .long("yellow")
                 .default_value("WARNING"),
         )
         .arg(
             Arg::with_name("red")
-                .help("String to match for red")
+                .help("Regex to match for red")
                 .short("r")
                 .long("red")
                 .default_value("ERROR"),
         )
         .arg(
             Arg::with_name("ready")
-                .help("String to search for to see if application is 'ready'")
+                .help("Regex to search for to see if application is 'ready'")
                 .short("d")
-                .long("ready"),
+                .long("ready")
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("pre")
                 .help("Chars to print in order (animated) until 'ready'")
                 .short("p")
                 .long("pre")
-                .default_value("\u{f250}\u{f251}\u{f252}\u{f253}"),
+                .default_value("|/-\\"),
+                //.default_value("\u{f250}\u{f251}\u{f252}\u{f253}"),
         )
         .arg(
             Arg::with_name("post")
                 .help("Chars to print in order (animated) after 'ready'")
                 .short("s")
                 .long("post")
-                .default_value("\u{2764}"),
+                .default_value("+"),
+                //.default_value("\u{2764}"),
         )
         .get_matches();
 
+    // maybe use regexSet here?
     let green = matches.value_of("green").expect("green not specified");
     let yellow = matches.value_of("yellow").expect("yellow not specified");
     let red = matches.value_of("red").expect("red not specified");
-    let ready = matches.value_of("ready");
+
+    let regexes = RegexSet::new(vec![green, yellow, red]).expect("bad color regex");
+
+    let ready = matches.value_of("ready").map(|s| {
+        Regex::new(s).expect("bad regex for ready")
+    });
     let pre_chars = matches.value_of("pre").expect("pre not specified");
     let post_chars = matches.value_of("post").expect("post not specified");
 
@@ -78,18 +89,18 @@ fn main() {
             break;
         }
 
-        let line = match buf {
-            ref s @ _ if buf.contains(green) => s.green(),
-            ref s @ _ if buf.contains(yellow) => s.yellow(),
-            ref s @ _ if buf.contains(red) => s.red().bold(),
-            ref s @ _ => s.normal(),
+        let line = match regexes.matches(&buf) {
+            ref m @ _ if m.matched(0) => buf.green(),
+            ref m @ _ if m.matched(1) => buf.yellow(),
+            ref m @ _ if m.matched(2) => buf.red().bold(),
+            _ => buf.normal(),
         };
 
-        let ready_char: Option<char> = if let Some(ready) = ready {
+        let ready_char: Option<char> = if let Some(ready) = ready.as_ref() {
             let mut ch = ready_chars.next();
 
             if !is_ready {
-                is_ready = buf.contains(ready);
+                is_ready = ready.is_match(&buf);
                 if ch.is_none() {
                     ready_chars = pre_chars.chars();
                     ch = ready_chars.next();
