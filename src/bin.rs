c144e8f818fd94a7a127d789baf8de468ec5ccfc
Benjamin Fry <benjamin.fry@salesforce.com>
2018-05-11 10:32:11 -0700

    add emojies

--- src/bin.rs
+++ src/bin.rs
@@ -18,21 +18,21 @@ fn main() {
                 .help("Regex to match for green")
                 .short("g")
                 .long("green")
-                .default_value("INFO"),
+                .default_value("(?:INFO)"),
         )
         .arg(
             Arg::with_name("yellow")
                 .help("Regex to match for yellow")
                 .short("y")
                 .long("yellow")
-                .default_value("WARNING"),
+                .default_value("(?:WARNING)"),
         )
         .arg(
             Arg::with_name("red")
                 .help("Regex to match for red")
                 .short("r")
                 .long("red")
-                .default_value("ERROR"),
+                .default_value("(?:ERROR)"),
         )
         .arg(
             Arg::with_name("ready")
@@ -46,16 +46,16 @@ fn main() {
                 .help("Chars to print in order (animated) until 'ready'")
                 .short("p")
                 .long("pre")
-                .default_value("|/-\\"),
-                //.default_value("\u{f250}\u{f251}\u{f252}\u{f253}"),
+                .default_value("😣"),
+            //.default_value("\u{f250}\u{f251}\u{f252}\u{f253}"),
         )
         .arg(
             Arg::with_name("post")
                 .help("Chars to print in order (animated) after 'ready'")
                 .short("s")
                 .long("post")
-                .default_value("+"),
-                //.default_value("\u{2764}"),
+                .default_value("😌"),
+            //.default_value("\u{2764}"),
         )
         .get_matches();
 
@@ -66,9 +66,9 @@ fn main() {
 
     let regexes = RegexSet::new(vec![green, yellow, red]).expect("bad color regex");
 
-    let ready = matches.value_of("ready").map(|s| {
-        Regex::new(s).expect("bad regex for ready")
-    });
+    let ready = matches
+        .value_of("ready")
+        .map(|s| Regex::new(s).expect("bad regex for ready"));
     let pre_chars = matches.value_of("pre").expect("pre not specified");
     let post_chars = matches.value_of("post").expect("post not specified");
 
@@ -84,8 +84,17 @@ fn main() {
     let mut is_ready = false;
     let mut ready_chars = pre_chars.chars();
 
-    while let Ok(length) = input.read_line(&mut buf) {
+    loop {
+        let length = match input.read_line(&mut buf) {
+            Err(error) => {
+                println!("broken pipe?: {}", error);
+                break;
+            }
+            Ok(length) => length,
+        };
+
         if length == 0 {
+            println!(">end of transmition<");
             break;
         }
 
@@ -117,8 +126,9 @@ fn main() {
             None
         };
 
-        write!(&mut output, "{}{}", ready_char.unwrap_or(' '), line)
+        write!(&mut output, "{} {}", ready_char.unwrap_or(' '), line,)
             .expect("error writing to standard out");
+        output.flush().expect("failed to flush stream");
         buf.clear();
     }
 }
